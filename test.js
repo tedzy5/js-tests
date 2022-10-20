21a7a113875b6aa340fce74ea723dde56ca1db8d
Theodor Abi-Mansour <theodor@abimansour.com>
2022-10-20 23:01:28 +0300

    updating classes and arrays

--- test.js
+++ test.js
@@ -2,3 +2,19 @@ let names = [];
 let families = [];
 let fullNames = [];
 
+console.info('Hello World');
+console.error('Hello Error');
+console.log('Hello log');
+console.dir('hello dir');
+
+function Position(firstName, lastName, position) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.position = position;
+}
+
+Position.prototype.fullName = function() { return `${this.firstName} ${this.lastName}` }
+
+const newPost = new Position('Theodor', 'Abo-Mansour', 'CEO');
+
+console.log(newPost);
